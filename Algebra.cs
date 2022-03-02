aad025c22586cf0350ce379e100fbf1ba54ded12
PrameelaDeepa <100566498+PrameelaDeepa@users.noreply.github.com>
2022-03-02 12:22:20 +0100

    calc new

---
+++ Algebra.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace CalcClassLibrary
+{
+    public class Algebra
+    {
+        public int Add(int a, int b)
+        { return a + b; }
+        public int Substract(int a, int b)
+        { return a - b; }
+        public int Multiply(int a, int b)
+        { return a * b; }
+        public float Divide(int a, int b)
+        { return a / b; }
+    }
+}
