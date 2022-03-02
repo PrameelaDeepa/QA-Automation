ec18dac6bca07ca37676c5b787e43ebf96ca0906
PrameelaDeepa <100566498+PrameelaDeepa@users.noreply.github.com>
2022-03-02 10:10:54 +0100

    Calc

--- QA-Automation/Calculator.cs
+++ QA-Automation/Calculator.cs
@@ -1,5 +1,4 @@
-﻿
-using CalcClassLibrary;
+﻿using CalcClassLibrary;
 
 namespace Caliculator;
 public class Program
@@ -8,13 +7,13 @@ public class Program
 
 
 	{
-        Algebra alg = new Algebra();
+		Algebra alg = new Algebra();
 		Console.WriteLine("Enter two numbers");
 		int a = Convert.ToInt32(Console.ReadLine());
 		int b = Convert.ToInt32(Console.ReadLine());
 
-		Console.WriteLine("Sum "+alg.Add(a, b));
-		Console.WriteLine("Substract "+ alg.Substract(a, b));
+		Console.WriteLine("Sum " + alg.Add(a, b));
+		Console.WriteLine("Substract " + alg.Substract(a, b));
 		Console.WriteLine("multiply" + alg.Multiply(a, b));
 		Console.WriteLine("Divide " + alg.Divide(a, b));
 
