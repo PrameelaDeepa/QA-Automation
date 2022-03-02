906c6a5b4a37b88a7441b932060c6d8571f63b0e
PrameelaDeepa <100566498+PrameelaDeepa@users.noreply.github.com>
2022-03-02 09:06:44 +0100

    Calc

---
+++ Caliculator/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using CalcClassLibrary;
+
+namespace Caliculator;
+public class Program
+{
+	public static void Main(string[] args)
+
+
+	{
+        Algebra alg = new Algebra();
+		Console.WriteLine("Enter two numbers");
+		int a = Convert.ToInt32(Console.ReadLine());
+		int b = Convert.ToInt32(Console.ReadLine());
+
+		Console.WriteLine("Sum "+alg.Add(a, b));
+		Console.WriteLine("Substract "+ alg.Substract(a, b));
+		Console.WriteLine("multiply" + alg.Multiply(a, b));
+		Console.WriteLine("Divide " + alg.Divide(a, b));
+
+	}
+}
+
