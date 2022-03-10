7b0ea1a90dc77790cb45889451e0815740a9089b
PrameelaDeepa <100566498+PrameelaDeepa@users.noreply.github.com>
2022-03-10 21:16:40 +0100

    algebra array1

--- Algebra.cs
+++ Algebra.cs
@@ -5,15 +5,26 @@ namespace QAAutomation
 {
     public class Algebra
     {
+        //Initialization of variables 
         int A;
         int B;
+
+        //default constructor 
+        public Algebra()
+        {
+            
+        }
+       
+        //constructor with 2 parameters 
         public Algebra(int a, int b)
         {
             this.A = a;
             this.B = b;
         }
         
-            public   int Add(int a, int b)
+        
+        // Methods intitialization
+        public   int Add(int a, int b)
             { return a + b; }
             public  int Substract(int a, int b)
             { return a - b; }
--- Calculator.cs
+++ Calculator.cs
@@ -9,17 +9,37 @@ namespace QAAutomation
 
 
          {
-            
-            Console.WriteLine("Enter two numbers ");
+            Console.WriteLine("Enter the first number ");
             int A = Convert.ToInt32(Console.ReadLine());
-           int B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number ");
+            int B = Convert.ToInt32(Console.ReadLine());
 
-            var alg1 = new Algebra(A , B);
 
+            //instantiating parametarized constructor object 
+            var alg1 = new Algebra();
+            Console.WriteLine(alg1.Add(A,B));
+            Console.WriteLine(alg1.Substract(A,B));
+
+            // var alg2 = new Algebra(A , B);
+
+            
+
+            // Array constructor
+            Algebra[] alg2 = new Algebra[2];
+            alg2[0] = new Algebra();
+            alg2[1] = new Algebra(A,B);
+ 
             Console.WriteLine("ADD : " + alg1.Add(A,B));
             Console.WriteLine("Substract : " + alg1.Substract(A,B));
             Console.WriteLine("Multiply : " + alg1.Multiply(A,B));
             Console.WriteLine("Divide : " + alg1.Divide(A,B));
+
+            Console.WriteLine("USing array constructor ");
+
+            Console.WriteLine("ADD : " + alg2[1].Add(A, B));
+            Console.WriteLine("Substract : " + alg2[1].Substract(A, B));
+            Console.WriteLine("Multiply : " + alg2[1].Multiply(A, B));
+            Console.WriteLine("Divide : " + alg2[1].Divide(A, B));
 
 
 
